8071752953f971dee97b9dd3cd25e4cce713c005
Ishani Nagda <55781581+IshaniNagda@users.noreply.github.com>
2021-06-25 10:07:15 +0530

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,9 @@
+#Given a non-empty sequence of characters str, return true if sequence is Binary, else return false
+
+def isBinary(str):
+    
+    for i in str:
+        if i not in "01":
+            return 0
+        
+    return 1
